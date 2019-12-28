99dcaa36b1c2ce986f12c1168d17f834f5d405a7
Vinay Sudrik <vinaysudrik@gmail.com>
2019-12-28 15:16:50 +0530

    commit on ui changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.contrib import messages
-from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
@@ -7,7 +8,11 @@ from django.http import HttpResponse
 @login_required(login_url='/login') 
 def dashboard(request):
     if request.user.is_staff:
-        return render(request, 'dashboard/admin.html')
+        instance = get_object_or_404(User, id=request.user.id)
+        context = {
+            'users':instance
+        }
+        return render(request, 'dashboard/admin.html', context)
     else:
         messages.warning(request, f'Not an admin')
         return redirect('/')
